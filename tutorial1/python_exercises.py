48a53a7a02b0a6a8e187b251dc08681b9bb5a393
yshivam <yshivam@umich.edu>
2020-10-06 20:55:57 -0400

    Finished python exercises

--- tutorial1/python_exercises.py
+++ tutorial1/python_exercises.py
@@ -6,12 +6,20 @@ def is_odd(x):
     """
     returns True if x is odd and False otherwise
     """
+    if x % 2 == 1:
+        return True
+    else:
+        return False
 
 
 def is_palindrome(word):
     """
     returns whether `word` is spelled the same forwards and backwards
     """
+    
+    if word == word[::-1]:
+        print(word)
+        return word
 
 
 def only_odds(numlist):
@@ -20,7 +28,11 @@ def only_odds(numlist):
 
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
-
+    list = []
+    for x in numlist:
+        if x%2 == 1:
+            list.append(x)
+    print(list)
 
 def count_words(text):
     """
@@ -35,3 +47,22 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+    
+    li = list(text.split(" "))
+    frequency = { }
+    
+    for item in li:
+       frequency[item] = li.count(item)
+       
+    print(frequency)
+    
+
+"""
+TESTING:
+
+is_odd(3)
+is_palindrome("dad")
+only_odds([1, 2, 3, 4, 5, 6])
+count_words("How much wood would a woodchuck chuck"
+            " if a woodchuck could chuck wood?")
+"""
